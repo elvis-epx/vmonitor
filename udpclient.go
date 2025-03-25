b1df0dcd35654da78cfd514e33ebbe886f9a0103
Elvis Pfützenreuter <epxx@epxx.co>
2025-03-25 17:04:41 -0300

    udpclient

--- udpclient.go
+++ udpclient.go
@@ -5,43 +5,58 @@ import (
 	"net"
 	"os"
     "time"
+    "log"
 )
+
+type Event struct {
+    name string
+    payload []byte
+}
 
 type Timeout struct {
     to time.Duration
     impl *time.Timer
-    ch chan string
+    ch chan Event
+    msg string
 }
 
-func NewTimeout(to time.Duration, ch chan string) (Timeout) {
-    timeout := Timeout{to, nil, ch}
+func NewTimeout(to time.Duration, ch chan Event, msg string) (Timeout) {
+    timeout := Timeout{to, nil, ch, msg}
     timeout.start()
     return timeout
 }
 
 func (timeout Timeout) start() {
     timeout.impl = time.AfterFunc(timeout.to, func() {
-        timeout.ch <- "timeout"
+        timeout.ch <- Event{timeout.msg, nil}
     })
 }
 
-func readudp(conn *net.UDPConn, ch chan string) {
+func (timeout Timeout) stop() (bool) {
+    return timeout.impl.Stop()
+}
 
-    var data [512]byte
-    _, addr, err := conn.ReadFromUDP(data[0:])
-    if err != nil {
-        fmt.Println(err)
-        ch <- "readerr"
-        return
+func (timeout Timeout) restart() (bool) {
+    return timeout.impl.Reset(timeout.to)
+}
+
+func (timeout Timeout) reset(to time.Duration) (bool) {
+    timeout.to = to
+    return timeout.restart()
+}
+
+func readudp(conn *net.UDPConn, ch chan Event, evtname string) {
+    for {
+        data := make([]byte, 512, 512)
+        n, _, err := conn.ReadFromUDP(data[0:])
+        if err != nil {
+            log.Fatal(err)
+        }
+        ch <- Event{evtname, data[0:n]}
     }
-
-	// Print the data read from the connection to the terminal
-	fmt.Print("> ", addr, " ", string(data[0:]))
-    ch <- "read"
 }
 
 func main() {
-
 	if len(os.Args) < 3 {
 		fmt.Println("Please provide host:port to bind, and host:port to connect to")
 		os.Exit(1)
@@ -66,20 +81,19 @@ func main() {
 	conn, err := net.ListenUDP("udp", udpLocalAddr)
 
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		fmt.Fatal(err)
 	}
 
 	// Send a message to the server
 	_, err = conn.WriteToUDP([]byte("Hello UDP Server\n"), udpRemoteAddr)
 	fmt.Println("send...")
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+        log.Print(err)
 	}
 
-    ch := make(chan string)
-    NewTimeout(5 * time.Second, ch)
-    go readudp(conn, ch)
-    <-ch
+    ch := make(chan Event)
+    NewTimeout(5 * time.Second, ch, "timeout1")
+    go readudp(conn, ch, "read1")
+    event := <-ch
+	fmt.Println(event)
 }
